564e1b87500aa50f7da69097229bf8967c5d1f65
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-10-20 14:07:45 -0700

    thought route draft. update if needed

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,12 +9,45 @@ module.exports = {
             console.error(err);
             res.status(500).json(err);
         }
+    },
+    async getThoughtById(req, res) {
+        try {
+            const dbThoughtData = await Thought.findOne({ _id: req.params.id })
+            .select("-__v");
+
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: "No thought with this id!" });
+            }
+            res.json(dbThoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createThought(req, res) {
+        try {
+            const { thoughtText, username, userId } = req.body;
+            const dbThoughtData = await Thought.create({ thoughtText, username, userId });
+            await User.findOneAndUpdate(
+                { _id: userId },
+                { $push: { thoughts: dbThoughtData._id } },
+                { new: true }
+            );
+            res.json(dbThoughtData);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
     }
+
     
     
     
-    
-    
+    // example json body for creating a thought:
+    // {
+    //     "thoughtText": "Here's a cool thought...",
+    //     "username": "lernantino",
+    //     "userId": "5edff358a0fcb779aa7b118b"
+    //   }
     
     
     
---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThoughtById,
+    deleteThoughtById,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+
+// /api/thoughts
+
+router.route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+
+// /api/thoughts/:id
+
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThoughtById)
+    .delete(deleteThoughtById);
