f0390ae412d2289efaa60a262cd83d2bb0941a1b
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-10-20 11:31:19 -0700

    reaction schema draft

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+// reaction will be a schema only used by the Thought model
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date, // uses a default value of the current timestamp
+      default: Date.now, // defaults the value to a function that returns the current timestamp
+      get: (timestamp) => dateFormat(timestamp), // uses a getter method to format the timestamp on query
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
