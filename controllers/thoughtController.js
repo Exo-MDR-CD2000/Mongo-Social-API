162d88e9eb33823e5ea99a0997935dff13394433
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-10-21 17:54:52 -0700

    added last 4 routes for thoughts and reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -38,36 +38,98 @@ module.exports = {
             console.error(err);
             res.status(500).json(err);
         }
-    }
+    },
     // example json body for creating a thought:
     // {
     //     "thoughtText": "Here's a cool thought...",
     //     "username": "lernantino",
     //     "userId": "5edff358a0fcb779aa7b118b"
     //   }
+    async updateThoughtById(req, res) { // route for this is /api/thoughts/:id
+        try {
+            const dbThoughtData = await Thought.findOneAndUpdate({ _id: req.params.id}, req.body, 
+                {new: true,
+                runValidators: true
+            }).select("-__v");
+
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: "No thought with this id!" });
+            }
+            res.json(dbThoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+
+        }
+    },
+
+    // to update a thought in insomnia:
+    // {
+    //     "thoughtText": "Here's a cool thought...",
+    //     "username": "lernantino",
+    //     "userId": "5edff358a0fcb779aa7b118b"
+    //   }
+
+    async deleteThoughtById(req, res) { // route for this is /api/thoughts/:id}
+        try {
+            const dbThoughtData = await Thought.findOneAndDelete({ _id: req.params.id })
+            .select("-__v");
+
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: "No thought with this id!" });
+            }
+
+            await User.findOneAndUpdate(
+                { username: dbThoughtData.username },
+                { $pull: { thoughts: req.params.id } }
+            );
+            res.json(dbThoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
     
-    
-    
-    
-    
-    //TODO: add routes for updating and deleting thoughts
-
-
-
-
-
-
-
-
-
-
-
-
-
     // After that, add routes for posting new reactions to thoughts and deleting reactions from thoughts.
     
-    
-    
+    async addReaction(req, res) { // route for this is /api/thoughts/:id/reactions
+        try {
+            const dbThoughtData = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId }, // This WORKS!!
+                { $addToSet: { reactions: req.body } },
+                { runValidators: true, new: true }
+            ).select("-__v");
+
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: "No thought with this id!" });
+            }
+            res.json(dbThoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // the json body for adding a reaction is:
+    // {
+    //     "reactionBody": "Here's a cool reaction...",
+    //     "username": "lernantino"
+    //   }
+
+
+    async deleteReactionById(req, res) { // route for this is /api/thoughts/:id/reactions/:reactionId
+        try {
+            const dbThoughtData = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                { $pull: { reactions: { reactionId: req.params.reactionId } } },
+                { runValidators: true, new: true }
+            ).select("-__v");
+
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: "No thought with this id!" });
+            }
+            res.json(dbThoughtData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
     
     
     
