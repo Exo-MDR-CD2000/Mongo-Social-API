0e6573249ab5391ee1f40f31351de2c720007978
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-10-24 14:03:38 -0700

    add simple data and seed for users, thoughts WIP

--- utils/data.js
+++ utils/data.js
@@ -1,25 +1,56 @@
-const usernames = [
-    'johndoe',
-    'janedoe',
-    'axlrose',
-    'slash',
-    'duffmckagan',
-    'izzystradlin',
-    'stevenadler',
-    'gilbyclarke',
-    'johnnythunders',
-    'Beatles87',
-    'RollingStones',
-    'DarkSideOfTheMoon',
-    'PinkFloyd',
-    'TearsFF97',
-]
-// 14 usernames
+function generateUsers() {
+  const users = [
+    {
+      username: "johndoe",
+      email: "john33@gmail.com",
+    },
+    {
+      username: "janedoe",
+      email: "jane55@gmail.com",
+    },
+    {
+      username: "axlrose",
+      email: "axlRose99@gmail.com",
+    },
+    {
+      username: "exoticjoe",
+      email: "exotic66@gmail.com",
+    },
+  ];
 
-const users = usernames.map(username => ({
-  username,
-  email: `${username}@gmail.com`
-}));
+  console.log('users seeded', users)
+  return users;
+}
 
 
-module.exports = users;
+//TODO: fix generateThoughts function
+
+
+function generateThoughts(users) { // currently not working
+  const thoughts = [];
+
+  users.forEach((user) => {
+    const thought = {
+      thoughtText: `This is ${user.username}'s first thought.`,
+      username: user.username,
+      createdAt: new Date(),
+      reactions: [],
+      userId: users.find(user => user.username === thought.username)._id
+    };
+
+    thoughts.push(thought);
+  });
+
+  return thoughts;
+}
+
+module.exports = { generateUsers, generateThoughts };
+
+
+
+    // example json body for creating a thought:
+    // {
+    //     "thoughtText": "Here's a cool thought...",
+    //     "username": "lernantino",
+    //     "userId": "5edff358a0fcb779aa7b118b"
+    //   }
--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,36 @@
-const { users } = require('../utils/data');
-const User = require('../models/User');
+const connection = require('../config/connection');
+const { generateUsers, generateThoughts } = require('./data');
+const { User, Thought } = require('../models');
 
 
 
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  await User.deleteMany({});
+  await Thought.deleteMany({});
+
+  await User.insertMany(generateUsers());
+
+  console.log('users seeded');
+
+  //const thoughts = await Thought.insertMany(generateThoughts(users));
+  //const thoughts = await Thought.insertMany(generateThoughts(users).map(thought => ({...thought, userId: users.find(user => user.username === thought.username)._id})));
+
+    //await Thought.collection.insertMany(generateThoughts(users).map(thought => ({...thought, userId: users.find(user => user.username === thought.username)._id})));
+
+    // await Thought.collection.insertOne({
+    //     thoughtText: "This is John Doe's first thought.",
+    //     username: "johndoe",
+    //     createdAt: new Date(),
+    //     reactions: [],
+    //     userId: users.find(user => user.username === "johndoe")._id
+    // })
+    
+
+    //TODO: later fix how to insert thoughts with the correct userId
+
+  console.log('thoughts seeded');
+
+  process.exit(0);
+});
